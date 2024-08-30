93bd93a7fc3cbdb2352278619f493cc732506251
mjndfreak <cagdasergencc@gmail.com>
2024-08-31 00:00:47 +0300

    Update SQL5-6-7.sql
    Added 9

--- SQL5-6-7.sql
+++ SQL5-6-7.sql
@@ -180,6 +180,22 @@ DELETE FROM employee WHERE email = 'clear2n@eepurl.com';
 
 SELECT * FROM employee;
 
+--SQL EXERCISE 9
+
+SELECT city.city, country.country
+FROM city
+INNER JOIN country
+ON city.country_id = country.country_id;
+
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id;
+
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+INNER JOIN public.customer ON customer.customer_id = rental.customer_id
+
 
 
 
